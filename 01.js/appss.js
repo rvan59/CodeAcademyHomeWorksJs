85e25fc79ec91c6e87116918c0cf13ec44b9f633
rvan59 <muxdarovrvan26@gmail.com>
2023-11-02 10:16:12 -0700

    yeni task yaradildi

--- 01.js/appss.js
+++ 01.js/appss.js
@@ -1,11 +1,34 @@
+// task2
+// let reqem = 34;
 
-// // task3
-// let count = 4;
-// let f=1;
-//   for (let i = 1; i < count; i++){  
-//         f= f*i
-//       } 
-//       console.log("fact", f);
+// for (let i = 0; i <= reqem; i++) {
+//   if (reqem % i === 0) {
+//     console.log(i);
+//   }
+// }
+
+// // task4
+// for(let i=1; i<10; i++){
+//   console.log(` ${i}*${i}=${i*i}`);
+// }
+
+// task5
+console.log(`i    i^2   i^3`);
+for (let i=1; i <= 10; i++) {
+  console.log(` ${i} ${i**2} ${i**3}`);
+}
+
+// task3
+// let reqem=35
+// let i =0
+// for (let i = 0; i <= reqem; i++) {
+//   if (reqem % i === 0) {
+//  reqem++
+//   }
+// }
+// console.log(i);
+
+
 
 // // task7
 // let number=0;

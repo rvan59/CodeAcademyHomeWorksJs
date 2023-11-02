973cdd0f8f01fdec45acbfd6caac7bb09d3a42ce
rvan59 <muxdarovrvan26@gmail.com>
2023-11-02 06:21:52 -0700

    yeni task yaradildi

---
+++ 01.js/appss.js
@@ -0,0 +1,14 @@
+
+// // task3
+// let count = 4;
+// let f=1;
+//   for (let i = 1; i < count; i++){  
+//         f= f*i
+//       } 
+//       console.log("fact", f);
+
+// // task7
+// let number=0;
+// for(let i=0;i<100; i++){
+//     number+=i;}
+//     console.log("Number",number);

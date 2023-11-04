c9963fbe7771ab7f694df2d95f8efb6334ce09ff
rvan59 <muxdarovrvan26@gmail.com>
2023-11-04 04:11:52 -0700

    task yaradildi

--- 02.Array&Object/appss.js
+++ 02.Array&Object/appss.js
@@ -1,22 +1,21 @@
-let arr = [3, 8, 5, 2, 7, 10, , 6];
-arr.forEach((item,inde) => {
- if((inde%2)-1){
-    console.log("Tek", item);
- }
-});
-
-arr.forEach((item,index) => {
-if(index%2){
-    console.log("Cut", item);
-}
-});
-
-
-// // satk3
-// let arr = [3, 8, 2, 5, 4, 10, 7, 6];
-// let max=arr[0];
-// for (let i=1; i<arr.length; i++){
-//   if(arr[i]>max){
-//     max=arr[i];
+// let arr = [3, 8, 5, 2, 7, 10, , 6];
+// arr.forEach((item, inde) => {
+//   if ((inde % 2) - 1) {
+//     console.log("Tek", item);
 //   }
-// }console.log("MAX" , max);
+// });
+
+// arr.forEach((item, index) => {
+//   if (index % 2) {
+//     console.log("Cut", item);
+//   }
+// });
+
+// satk3
+let arr = [3, 8, 2, 5, 4, 10, 7, 6];
+let max=arr[0];
+for (let i=1; i<arr.length; i++){
+  if(arr[i]>max){
+    max=arr[i];
+  }
+}console.log("Max" , max);

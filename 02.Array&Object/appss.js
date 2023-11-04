fd5c572fbb42b6fce67c1cf750e8b3637ea8ccaa
rvan59 <muxdarovrvan26@gmail.com>
2023-11-04 00:16:10 -0700

    yeni task yaradildi

--- 02.Array&Object/appss.js
+++ 02.Array&Object/appss.js
@@ -1,4 +1,16 @@
+let arr = [3, 8, 2, 5, 4, 10, 7, 6];
+let odd = 0;
+let even = 0;
+for (let i = 0; i <= arr.length; i++) {
+  if (i % 2 === 0) {
+    even += i;
+  } else {
+    odd += i;
+  }
+}
 
+console.log("ODD: ", odd);
+console.log("EVEN: ", even);
 
 
 

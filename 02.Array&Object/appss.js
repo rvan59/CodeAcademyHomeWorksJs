1c9aac1883f3829b5d44afef55dca9ca1dcd3cfb
rvan59 <muxdarovrvan26@gmail.com>
2023-11-04 00:25:58 -0700

    yeni task yaradildi

--- 02.Array&Object/appss.js
+++ 02.Array&Object/appss.js
@@ -1,18 +1,15 @@
-let arr = [3, 8, 2, 5, 4, 10, 7, 6];
-let odd = 0;
-let even = 0;
-for (let i = 0; i <= arr.length; i++) {
-  if (i % 2 === 0) {
-    even += i;
-  } else {
-    odd += i;
-  }
+let arr = [3, 8, 5, 2, 7, 10, , 6];
+arr.forEach((item,inde) => {
+ if((inde%2)-1){
+    console.log("Tek", item);
+ }
+});
+
+arr.forEach((item,index) => {
+if(index%2){
+    console.log("Cut", item);
 }
-
-console.log("ODD: ", odd);
-console.log("EVEN: ", even);
-
-
+});
 
 
 // // satk3
@@ -23,4 +20,3 @@ console.log("EVEN: ", even);
 //     max=arr[i];
 //   }
 // }console.log("MAX" , max);
-

0c6e6006af91dc5164ead51696e9bc8ba0f69e9e
rvan59 <muxdarovrvan26@gmail.com>
2023-11-06 12:37:06 -0800

    yeni task yaradildi

---
+++ oyernmek/appss.js
@@ -0,0 +1,31 @@
+// function vurma(a,b){
+//     return a*b;
+// }
+// let sonuc= vurma(3,4);
+// console.log("cvb:"+ sonuc);
+
+// function hesabla(a,b){
+//     return a+b;
+// }
+// let cvb=hesabla(7,9);
+// console.log("cvb:"+cvb);
+
+// function number (  a ,b){
+//     return  (a+b);
+// }
+// let toplam=number( 8 ,9);
+// console.log("cvb;"+ toplam);
+
+// let xan=(a,b) => 2*(a+b);
+// console.log(xan(3,4));
+
+// let num = number=>number**2;
+// console.log(num(9));
+
+// function net(){
+//     console.log(arguments);
+// }
+
+// console.log(net(1,2,3,4));
+// console.log(net(1,2,3,4,5,6));
+// console.log(net(1,2,3,4,5,6,7,8,9));

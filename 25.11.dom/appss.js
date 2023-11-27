d519b2d6f60faf13b6e3bf3f7641dd08abbe52d4
rvan59 <muxdarovrvan26@gmail.com>
2023-11-27 02:23:40 -0800

    yeni task

--- 25.11.dom/appss.js
+++ 25.11.dom/appss.js
@@ -1,6 +1,9 @@
-let nav =document.querySelector("nav");
-let header=document.querySelector("header");
+let nav = document.querySelector("nav");
+let header = document.querySelector("header");
 
-window.addEventListener("scroll", function(){
-    console.log(window,screenY);
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+  window.scrollY > 0
+    ? header.classList.add("header.scroll")
+    : header.classList.remove("header.scroll");
 });

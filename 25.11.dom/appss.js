a5c24667b21dac8be2f36ac72922bd80b730cc47
rvan59 <muxdarovrvan26@gmail.com>
2023-11-29 01:49:44 -0800

    oyrenemek

--- 25.11.dom/appss.js
+++ 25.11.dom/appss.js
@@ -1,9 +1,7 @@
-let nav = document.querySelector("nav");
-let header = document.querySelector("header");
+const lists = document.querySelectorAll(`nav ul li`);
+window.onscroll = function() {
 
-window.addEventListener("scroll", function () {
-  console.log(window.scrollY);
-  window.scrollY > 0
-    ? header.classList.add("header.scroll")
-    : header.classList.remove("header.scroll");
-});
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      console.log("Səhifə scroll edir!");
+  }
+};
